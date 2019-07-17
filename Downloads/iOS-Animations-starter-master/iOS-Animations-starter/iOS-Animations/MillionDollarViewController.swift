b6d2bef20c5d2fb6554f2698fdc21b685d7f37fa
Débora <deborajuliane@gmail.com>
2019-07-17 17:16:36 -0300

    teste 2 debora

--- Downloads/iOS-Animations-starter-master/iOS-Animations-starter/iOS-Animations/MillionDollarViewController.swift
+++ Downloads/iOS-Animations-starter-master/iOS-Animations-starter/iOS-Animations/MillionDollarViewController.swift
@@ -1,36 +1,4 @@
 // TESTANDO O GIT
 //teste Paulo
 //teste fernanda
-//  MillionDollarViewController.swift
-//  iOS-Animations
-//
-//  Created by Barney on 07/02/2018.
-//  Copyright © 2018 Barney. All rights reserved.
-//
-
-import UIKit
-
-class MillionDollarViewController: UIViewController, UITextFieldDelegate {
-
-    @IBOutlet weak var millionDollarLabel: UILabel!
-    @IBOutlet weak var emailTextField: UITextField!
-    @IBOutlet weak var passwordTextField: UITextField!
-
-    @IBAction func loginButton(_ sender: Any) {
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        emailTextField.delegate = self
-        passwordTextField.delegate = self
-    }
-
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-
-        textField.resignFirstResponder()
-
-        return true
-    }
-}
-
+// teste 2 débora
